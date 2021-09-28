e7623052092b33830311d61082c105336c16f590
TakenouchiTR <draconianlds@gmail.com>
2021-09-28 18:31:17 -0400

    Initial commit

---
+++ SpaceInvaders/Model/GameManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace SpaceInvaders.Model
+{
+    /// <summary>
+    ///     Manages the entire game.
+    /// </summary>
+    public class GameManager
+    {
+        #region Data members
+
+        private const double PlayerShipBottomOffset = 30;
+        private readonly double backgroundHeight;
+        private readonly double backgroundWidth;
+
+        private PlayerShip playerShip;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GameManager" /> class.
+        ///     Precondition: backgroundHeight > 0 AND backgroundWidth > 0
+        /// </summary>
+        /// <param name="backgroundHeight">The backgroundHeight of the game play window.</param>
+        /// <param name="backgroundWidth">The backgroundWidth of the game play window.</param>
+        public GameManager(double backgroundHeight, double backgroundWidth)
+        {
+            if (backgroundHeight <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(backgroundHeight));
+            }
+
+            if (backgroundWidth <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(backgroundWidth));
+            }
+
+            this.backgroundHeight = backgroundHeight;
+            this.backgroundWidth = backgroundWidth;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     Initializes the game placing player ship and enemy ship in the game.
+        ///     Precondition: background != null
+        ///     Postcondition: Game is initialized and ready for play.
+        /// </summary>
+        /// <param name="background">The background canvas.</param>
+        public void InitializeGame(Canvas background)
+        {
+            if (background == null)
+            {
+                throw new ArgumentNullException(nameof(background));
+            }
+
+            this.createAndPlacePlayerShip(background);
+        }
+
+        private void createAndPlacePlayerShip(Canvas background)
+        {
+            this.playerShip = new PlayerShip();
+            background.Children.Add(this.playerShip.Sprite);
+
+            this.placePlayerShipNearBottomOfBackgroundCentered();
+        }
+
+        private void placePlayerShipNearBottomOfBackgroundCentered()
+        {
+            this.playerShip.X = this.backgroundWidth / 2 - this.playerShip.Width / 2.0;
+            this.playerShip.Y = this.backgroundHeight - this.playerShip.Height - PlayerShipBottomOffset;
+        }
+
+        /// <summary>
+        ///     Moves the player ship to the left.
+        ///     Precondition: none
+        ///     Postcondition: The player ship has moved left.
+        /// </summary>
+        public void MovePlayerShipLeft()
+        {
+            this.playerShip.MoveLeft();
+        }
+
+        /// <summary>
+        ///     Moves the player ship to the right.
+        ///     Precondition: none
+        ///     Postcondition: The player ship has moved right.
+        /// </summary>
+        public void MovePlayerShipRight()
+        {
+            this.playerShip.MoveRight();
+        }
+
+        #endregion
+
+
+    }
+}
