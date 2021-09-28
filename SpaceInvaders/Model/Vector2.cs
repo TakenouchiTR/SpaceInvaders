393b9c7c8c1fbe87f4139f8254577023eb48f254
TakenouchiTR <draconianlds@gmail.com>
2021-09-28 19:10:12 -0400

    Add Vector2 class

---
+++ SpaceInvaders/Model/Vector2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders.Model
+{
+    public class Vector2
+    {
+        #region Properties
+        public double X { get; set; }
+        public double Y { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public Vector2() : this(0)
+        {
+
+        }
+
+        public Vector2(double value) : this(value, value)
+        {
+
+        }
+
+        public Vector2(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        #endregion
+    }
+}
