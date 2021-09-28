193f5579bed36c7be650bef55a859485ec0923f7
TakenouchiTR <draconianlds@gmail.com>
2021-09-28 19:32:19 -0400

    Add update timer

--- SpaceInvaders/Model/GameManager.cs
+++ SpaceInvaders/Model/GameManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using SpaceInvaders.Model.Entities;
 
@@ -12,10 +14,15 @@ namespace SpaceInvaders.Model
         #region Data members
 
         private const double PlayerShipBottomOffset = 30;
+        private const double MillisecondsInSecond = 1000;
+
         private readonly double backgroundHeight;
         private readonly double backgroundWidth;
 
         private PlayerShip playerShip;
+        private readonly DispatcherTimer updateTimer;
+        private readonly HashSet<GameObject> gameObjects;
+        private long prevUpdateTime;
 
         #endregion
 
@@ -41,6 +48,27 @@ namespace SpaceInvaders.Model
 
             this.backgroundHeight = backgroundHeight;
             this.backgroundWidth = backgroundWidth;
+
+            this.updateTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(.1)
+            };
+            this.updateTimer.Tick += this.onUpdateTimerTick;
+            this.updateTimer.Start();
+
+        }
+
+        private void onUpdateTimerTick(object sender, object e)
+        {
+            long curTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            long timeSinceLastTick = curTime - this.prevUpdateTime;
+            double delta = timeSinceLastTick / MillisecondsInSecond;
+            this.prevUpdateTime = curTime;
+
+            foreach (var gameObject in this.gameObjects)
+            {
+                gameObject.Update(delta);
+            }
         }
 
         #endregion
