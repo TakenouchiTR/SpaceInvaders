340e39e1e79b5a602082d25c3f46403168b723ea
TakenouchiTR <draconianlds@gmail.com>
2021-09-29 13:29:56 -0400

    Set constant for movement speed

--- SpaceInvaders/Model/Entities/PlayerBullet.cs
+++ SpaceInvaders/Model/Entities/PlayerBullet.cs
@@ -5,21 +5,22 @@ namespace SpaceInvaders.Model.Entities
 {
     public class PlayerBullet : GameObject
     {
+        private const int travelSpeed = -1000;
+
         private Vector2 velocity;
 
         public PlayerBullet(GameManager parent) : base(parent, new PlayerBulletSprite())
         {
-            this.velocity = new Vector2(0, -100);
+            this.velocity = new Vector2();
             this.Monitoring = true;
-            this.Monitorable = true;
             this.CollisionMasks = (int) PhysicsLayer.Enemy;
             this.CollisionLayers = (int) PhysicsLayer.PlayerHitbox;
         }
 
         public override void Update(double delta)
         {
-            this.velocity.Y = -1000 * delta;
-            Move(this.velocity);
+            this.velocity.Y = travelSpeed * delta;
+            this.Move(this.velocity);
             if (this.Bottom < 0)
             {
                 this.QueueRemoval();
