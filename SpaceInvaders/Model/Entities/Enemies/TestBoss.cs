f0c96945ccb2170710925b63da7af9c4c91269e0
TakenouchiTR <draconianlds@gmail.com>
2021-10-01 12:03:39 -0400

    Add test boss

---
+++ SpaceInvaders/Model/Entities/Enemies/TestBoss.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+using SpaceInvaders.View.Sprites;
+
+namespace SpaceInvaders.Model.Entities.Enemies
+{
+    public class TestBoss : Enemy
+    {
+        private int health = 3;
+        private Vector2 velocity = new Vector2(75, 0);
+        private int direction = 1;
+
+        public TestBoss(GameManager gameManager) : base(gameManager, new TestBossSprite())
+        {
+            this.CollisionMasks = 0;
+            this.Monitorable = false;
+            this.CollisionLayers = (int) PhysicsLayer.Enemy;
+            
+            var leftTarget = new TestBossTarget(gameManager) 
+            {
+                Center = new Vector2(this.X, this.Bottom)
+            };
+            var rightTarget = new TestBossTarget(gameManager)
+            {
+                Center = new Vector2(this.Right, this.Bottom)
+            };
+            var centerTarget = new TestBossTarget(gameManager)
+            {
+                Center = new Vector2(this.Center.X, this.Bottom)
+            };
+
+            this.AddChild(leftTarget);
+            this.AddChild(centerTarget);
+            this.AddChild(rightTarget);
+
+            gameManager.QueueGameObjectForAddition(leftTarget);
+            gameManager.QueueGameObjectForAddition(centerTarget);
+            gameManager.QueueGameObjectForAddition(rightTarget);
+
+            leftTarget.Removed += onTargetRemoved;
+            centerTarget.Removed += onTargetRemoved;
+            rightTarget.Removed += onTargetRemoved;
+        }
+
+        private void onTargetRemoved(object sender, EventArgs e)
+        {
+            this.health -= 1;
+            if (this.health <= 0)
+            {
+                this.QueueRemoval();
+            }
+
+            if (sender is GameObject child)
+            {
+                child.Removed -= this.onTargetRemoved;
+            }
+        }
+
+        public override void Update(double delta)
+        {
+            Move(this.velocity * delta);
+            if (this.X < 0 || this.Right > gameManager.ScreenWidth)
+            {
+                this.velocity *= -1;
+            }
+        }
+
+        protected override void OnMovementTick(Vector2 moveDistance)
+        {
+
+        }
+    }
+}
---
+++ SpaceInvaders/Model/Entities/Enemies/TestBossTarget.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using SpaceInvaders.View.Sprites;
+
+namespace SpaceInvaders.Model.Entities.Enemies
+{
+    public class TestBossTarget : GameObject
+    {
+        private int health = 3;
+        private bool canShoot;
+        private DispatcherTimer shootToggleTimer;
+        private DispatcherTimer shootTimer;
+
+        public TestBossTarget(GameManager gameManager) : base(gameManager, new BasicEnemySprite())
+        {
+            this.Monitoring = true;
+            this.Monitorable = true;
+            this.CollisionLayers = (int) PhysicsLayer.Enemy;
+            this.CollisionMasks = (int) PhysicsLayer.PlayerHitbox;
+
+            this.canShoot = false;
+            this.shootToggleTimer = new DispatcherTimer();
+            this.shootToggleTimer.Interval = TimeSpan.FromSeconds(2);
+            this.shootToggleTimer.Tick += this.onShootToggleTimerTick;
+            this.shootToggleTimer.Start();
+
+            this.shootTimer = new DispatcherTimer();
+            this.shootTimer.Interval = TimeSpan.FromSeconds(.5);
+            this.shootTimer.Tick += this.onShooTimerTick;
+            this.shootTimer.Start();
+
+            this.Removed += this.onRemoved;
+        }
+
+        private void onRemoved(object sender, EventArgs e)
+        {
+            this.shootTimer.Stop();
+            this.shootToggleTimer.Stop();
+            this.shootTimer.Tick -= this.onShooTimerTick;
+            this.shootToggleTimer.Tick -= this.onShootToggleTimerTick;
+        }
+
+        private void onShooTimerTick(object sender, object e)
+        {
+            if (this.canShoot)
+            {
+                var bullet = new EnemyBullet(gameManager)
+                {
+                    Position = this.Position
+                };
+
+                gameManager.QueueGameObjectForAddition(bullet);
+            }
+        }
+
+        private void onShootToggleTimerTick(object sender, object e)
+        {
+            this.canShoot = !this.canShoot;
+        }
+
+        public override void Update(double delta)
+        {
+
+        }
+
+        public override void HandleCollision(GameObject target)
+        {
+            this.health--;
+            if (this.health <= 0)
+            {
+                this.QueueRemoval();
+
+            }
+        }
+    }
+}
