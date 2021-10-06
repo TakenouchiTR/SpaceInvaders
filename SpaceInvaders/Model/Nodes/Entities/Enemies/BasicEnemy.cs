fe552e1d731d4748342185212e49781e6f2251c6
TakenouchiTR <draconianlds@gmail.com>
2021-10-06 12:58:20 -0400

    Add enemy classes to project

---
+++ SpaceInvaders/Model/Nodes/Entities/Enemies/BasicEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceInvaders.View.Sprites;
+
+namespace SpaceInvaders.Model.Nodes.Entities.Enemies
+{
+    public class BasicEnemy : Enemy
+    {
+        public BasicEnemy() : base(new BasicEnemySprite())
+        {
+            this.Collision.Collided += this.onCollided;
+            this.Score = 10;
+        }
+
+        private void onCollided(object sender, CollisionArea e)
+        {
+            this.QueueForRemoval();
+        }
+    }
+}
---
+++ SpaceInvaders/Model/Nodes/Entities/Enemies/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using SpaceInvaders.View.Sprites;
+
+namespace SpaceInvaders.Model.Nodes.Entities.Enemies
+{
+    public abstract class Enemy : Entity
+    {
+        public int Score { get; protected set; }
+
+        protected Enemy(BaseSprite sprite) : base(sprite)
+        {
+            this.Collision.CollisionLayers = PhysicsLayer.Enemy;
+            this.Collision.CollisionMasks = PhysicsLayer.PlayerHitbox;
+            this.Collision.Monitoring = true;
+            this.Collision.Monitorable = true;
+            this.Score = 0;
+        }
+    }
+}
