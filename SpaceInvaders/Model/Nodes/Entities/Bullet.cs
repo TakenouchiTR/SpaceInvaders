e4fb3257956d5f40f632c5c6838ddc28db7236db
TakenouchiTR <draconianlds@gmail.com>
2021-10-06 11:46:43 -0400

    Add base bullet entity

---
+++ SpaceInvaders/Model/Nodes/Entities/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceInvaders.View.Sprites;
+
+namespace SpaceInvaders.Model.Nodes.Entities
+{
+    public class Bullet : Entity
+    {
+        public Vector2 Velocity { get; set; }
+        public Bullet() : base(new PlayerBulletSprite())
+        {
+            this.Velocity = new Vector2();
+            Collision.Monitoring = true;
+            Collision.Monitorable = true;
+        }
+
+        public override void Update(double delta)
+        {
+            this.Move(Velocity * delta);
+            if (this.IsOffScreen())
+            {
+                this.QueueForRemoval();
+            }
+            base.Update(delta);
+        }
+    }
+}
