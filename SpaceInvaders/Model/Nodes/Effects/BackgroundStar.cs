fe23232ac96db720701f8554dc3b88cafecc47fc
TakenouchiTR <draconianlds@gmail.com>
2021-10-07 10:46:27 -0400

    Add BackgroundStar to project

---
+++ SpaceInvaders/Model/Nodes/Effects/BackgroundStar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SpaceInvaders.View;
+using SpaceInvaders.View.Sprites;
+
+namespace SpaceInvaders.Model.Nodes.Effects
+{
+    public class BackgroundStar : SpriteNode
+    {
+        private static readonly Random StarRandom = new Random();
+        private const double MaxSpeed = 250;
+        private const double MinSpeed = 100;
+
+        private readonly double velocity;
+
+        public BackgroundStar(BaseSprite sprite) : base(new StarSprite())
+        {
+            this.velocity = StarRandom.NextDouble() * (MaxSpeed - MinSpeed) + MinSpeed;
+            this.setStartingPosition();
+        }
+
+        private void setStartingPosition()
+        {
+            this.Y = -this.Height;
+            this.X = StarRandom.Next((int)(MainPage.ApplicationWidth - this.Width));
+        }
+
+        public override void Update(double delta)
+        {
+            this.Y += this.velocity;
+            if (this.IsOffScreen())
+            {
+                this.QueueForRemoval();
+            }
+            base.Update(delta);
+        }
+    }
+}
