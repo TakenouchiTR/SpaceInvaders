6d0469499e0e33da02a0324888429edcb4ee9bd9
TakenouchiTR <draconianlds@gmail.com>
2021-10-12 13:48:05 -0400

    Add RenderLayer to project

---
+++ SpaceInvaders/Model/RenderLayer.cs
@@ -0,0 +1,19 @@
+﻿namespace SpaceInvaders.Model
+{
+    /// <summary>
+    ///     Represents which layer a sprite will be rendered on
+    /// </summary>
+    public enum RenderLayer
+    {
+        BackgroundBottom = 0,
+        BackgroundMiddle = 1,
+        BackgroundTop    = 2,
+        MainBottom       = 3,
+        MainLowerMiddle  = 4,
+        MainUpperMiddle  = 5,
+        MainTop          = 6,
+        UiBottom         = 7,
+        UiMiddle         = 8,
+        UiTop            = 9
+    }
+}
