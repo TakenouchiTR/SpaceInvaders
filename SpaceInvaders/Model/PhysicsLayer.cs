209dbc77260e7ee2226b93364467306a533c4e54
TakenouchiTR <draconianlds@gmail.com>
2021-10-20 17:23:42 -0400

    Add documentation
    Closes #1
    Closes #2

--- SpaceInvaders/Model/PhysicsLayer.cs
+++ SpaceInvaders/Model/PhysicsLayer.cs
@@ -8,10 +8,29 @@ namespace SpaceInvaders.Model
     [Flags]
     public enum PhysicsLayer
     {
+        /// <summary>
+        ///     Represents the collision of any item that is part of the world (Obstacles, cover, etc.).
+        /// </summary>
         World = 0b0000_0001,
+
+        /// <summary>
+        ///     Represents the collision of the player's body.
+        /// </summary>
         Player = 0b0000_0010,
+
+        /// <summary>
+        ///     Represents the collision of the enemies' bodies.
+        /// </summary>
         Enemy = 0b0000_0100,
+
+        /// <summary>
+        ///     Represents the collision of anything used by the player to damage the enemies.
+        /// </summary>
         PlayerHitbox = 0b0000_1000,
+
+        /// <summary>
+        ///     Represents the collision of anything used by the enemies to damage the player.
+        /// </summary>
         EnemyHitbox = 0b0001_0000
     }
 }
--- SpaceInvaders/Model/RenderLayer.cs
+++ SpaceInvaders/Model/RenderLayer.cs
@@ -1,19 +1,59 @@
 ﻿namespace SpaceInvaders.Model
 {
     /// <summary>
-    ///     Represents which layer a sprite will be rendered on
+    ///     Represents which layer a sprite will be rendered on.<br />
+    ///     Lower values will be rendered under higher values.
     /// </summary>
     public enum RenderLayer
     {
+        /// <summary>
+        ///     The bottom-most layer of the background layers.
+        /// </summary>
         BackgroundBottom = 0,
+
+        /// <summary>
+        ///     The middle of the background layers.
+        /// </summary>
         BackgroundMiddle = 1,
+
+        /// <summary>
+        ///     The top-most layer of the background layers.
+        /// </summary>
         BackgroundTop = 2,
+
+        /// <summary>
+        ///     The bottom-most layer of the main playing layers.
+        /// </summary>
         MainBottom = 3,
+
+        /// <summary>
+        ///     The second-lowest layer of the main playing layers.
+        /// </summary>
         MainLowerMiddle = 4,
+
+        /// <summary>
+        ///     The second-highest layer of the main playing layers.
+        /// </summary>
         MainUpperMiddle = 5,
+
+        /// <summary>
+        ///     The top-most layer of the main playing layers.
+        /// </summary>
         MainTop = 6,
+
+        /// <summary>
+        ///     The bottom-most layer of the UI layers.
+        /// </summary>
         UiBottom = 7,
+
+        /// <summary>
+        ///     The middle layer of the UI layers.
+        /// </summary>
         UiMiddle = 8,
+
+        /// <summary>
+        ///     The top-most layer of the UI layers.
+        /// </summary>
         UiTop = 9
     }
 }
