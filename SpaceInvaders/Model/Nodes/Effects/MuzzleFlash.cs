a64bee6a72adfbbf8998b4386f7f12a6720549c3
TakenouchiTR <draconianlds@gmail.com>
2021-10-20 22:13:42 -0400

    Add Muzzle Flash to project

---
+++ SpaceInvaders/Model/Nodes/Effects/MuzzleFlash.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+using SpaceInvaders.View.Sprites.Effects;
+
+namespace SpaceInvaders.Model.Nodes.Effects
+{
+    public class MuzzleFlash: SpriteNode
+    {
+        public MuzzleFlash() : base(new MuzzleFlashSprite(), RenderLayer.MainTop)
+        {
+            Timer removalTimer = new Timer(.1, false);
+            removalTimer.Start();
+            removalTimer.Tick += this.onRemovalTimerTick;
+            AttachChild(removalTimer);
+        }
+
+        private void onRemovalTimerTick(object sender, EventArgs e)
+        {
+            QueueForRemoval();
+        }
+    }
+}
---
+++ SpaceInvaders/View/Sprites/Effects/MuzzleFlashSprite.xaml.cs
@@ -0,0 +1,12 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.Effects
+{
+    public sealed partial class MuzzleFlashSprite
+    {
+        public MuzzleFlashSprite()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
