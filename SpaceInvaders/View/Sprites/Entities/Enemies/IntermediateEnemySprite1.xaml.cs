7a5dc2a93f08fed99a6f4043dd46687224fdf9d8
TakenouchiTR <draconianlds@gmail.com>
2021-10-22 22:02:01 -0400

    Add animation frames for IntermediateEnemy

---
+++ SpaceInvaders/View/Sprites/Entities/Enemies/IntermediateEnemySprite1.xaml.cs
@@ -0,0 +1,25 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.Entities.Enemies
+{
+    /// <summary>
+    ///     The first frame of IntermediateEnemy's animation
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.View.Sprites.BaseSprite" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector2" />
+    public sealed partial class IntermediateEnemySprite1
+    {
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="IntermediateEnemySprite1" /> class.
+        /// </summary>
+        public IntermediateEnemySprite1()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ SpaceInvaders/View/Sprites/Entities/Enemies/IntermediateEnemySprite2.xaml.cs
@@ -0,0 +1,25 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.Entities.Enemies
+{
+    /// <summary>
+    ///     The second frame for IntermediateEnemy's animation
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.View.Sprites.BaseSprite" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector2" />
+    public sealed partial class IntermediateEnemySprite2
+    {
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="IntermediateEnemySprite2" /> class.
+        /// </summary>
+        public IntermediateEnemySprite2()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
