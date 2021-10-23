7337ca3357747dbd9f8f2a97bfe2e4449cf41eb1
TakenouchiTR <draconianlds@gmail.com>
2021-10-22 20:29:01 -0400

    Add FullHeartSprite to project

---
+++ SpaceInvaders/View/Sprites/UI/FullHeartSprite.xaml.cs
@@ -0,0 +1,19 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.UI
+{
+    /// <summary>
+    /// Draws a full heart
+    /// </summary>
+    /// <seealso cref="Windows.UI.Xaml.Controls.UserControl" />
+    public sealed partial class FullHeartSprite
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FullHeartSprite"/> class.
+        /// </summary>
+        public FullHeartSprite()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
