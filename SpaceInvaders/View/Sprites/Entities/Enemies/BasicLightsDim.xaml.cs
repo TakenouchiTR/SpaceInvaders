c920981c6de3b09567f153d11331bfc7a354f2bc
TakenouchiTR <draconianlds@gmail.com>
2021-10-22 21:27:03 -0400

    Add documentation

--- SpaceInvaders/View/Sprites/Entities/Enemies/BasicLightsDim.xaml.cs
+++ SpaceInvaders/View/Sprites/Entities/Enemies/BasicLightsDim.xaml.cs
@@ -2,8 +2,17 @@
 
 namespace SpaceInvaders.View.Sprites.Entities.Enemies
 {
+    /// <summary>
+    /// Draws a pair of dimmed lights for Basic and Intermediate enemies.
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.View.Sprites.BaseSprite" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector2" />
     public sealed partial class BasicLightsDim
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicLightsDim"/> class.
+        /// </summary>
         public BasicLightsDim()
         {
             this.InitializeComponent();
--- SpaceInvaders/View/Sprites/Entities/Enemies/BasicLightsLit.xaml.cs
+++ SpaceInvaders/View/Sprites/Entities/Enemies/BasicLightsLit.xaml.cs
@@ -2,8 +2,17 @@
 
 namespace SpaceInvaders.View.Sprites.Entities.Enemies
 {
+    /// <summary>
+    /// Draws a pair of lit lights for Basic and Intermediate enemies.
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.View.Sprites.BaseSprite" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector2" />
     public sealed partial class BasicLightsLit
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicLightsLit"/> class.
+        /// </summary>
         public BasicLightsLit()
         {
             this.InitializeComponent();
