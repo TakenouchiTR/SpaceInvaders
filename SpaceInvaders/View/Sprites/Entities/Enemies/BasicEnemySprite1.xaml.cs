deaa4e5449e1d61a14ad695cea3c2ed061ca1400
TakenouchiTR <draconianlds@gmail.com>
2021-10-22 21:26:55 -0400

    Add BasicEnemy's animation frames to the project

---
+++ SpaceInvaders/View/Sprites/Entities/Enemies/BasicEnemySprite1.xaml.cs
@@ -0,0 +1,21 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.Entities.Enemies
+{
+    /// <summary>
+    /// The first frame of BasicEnemy's animation
+    /// </summary>
+    /// <seealso cref="Windows.UI.Xaml.Controls.UserControl" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector2" />
+    public sealed partial class BasicEnemySprite1
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicEnemySprite1"/> class.
+        /// </summary>
+        public BasicEnemySprite1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ SpaceInvaders/View/Sprites/Entities/Enemies/BasicEnemySprite2.xaml.cs
@@ -0,0 +1,21 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.Entities.Enemies
+{
+    /// <summary>
+    /// The second frame for BasicEnemy
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.View.Sprites.BaseSprite" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector" />
+    /// <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector2" />
+    public sealed partial class BasicEnemySprite2
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicEnemySprite2"/> class.
+        /// </summary>
+        public BasicEnemySprite2()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
