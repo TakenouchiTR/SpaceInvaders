beb1dfa852c8b4e09f81528846737245cbefedc1
TakenouchiTR <draconianlds@gmail.com>
2021-10-22 21:14:49 -0400

    Add Sprites to project

---
+++ SpaceInvaders/View/Sprites/Entities/Enemies/BasicLightsDim.xaml.cs
@@ -0,0 +1,12 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.Entities.Enemies
+{
+    public sealed partial class BasicLightsDim
+    {
+        public BasicLightsDim()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
