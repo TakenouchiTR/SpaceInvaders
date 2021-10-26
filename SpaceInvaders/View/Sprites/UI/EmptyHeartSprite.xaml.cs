1b55438bcb403e0a881d786c59cf57a1ba891e32
TakenouchiTR <draconianlds@gmail.com>
2021-10-26 18:55:04 -0400

    Add EmptyHeartSprite to project

---
+++ SpaceInvaders/View/Sprites/UI/EmptyHeartSprite.xaml.cs
@@ -0,0 +1,12 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.UI
+{
+    public sealed partial class EmptyHeartSprite
+    {
+        public EmptyHeartSprite()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
