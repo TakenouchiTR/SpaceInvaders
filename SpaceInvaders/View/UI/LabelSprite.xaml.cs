428b17a93b83acbcf5982bc884c11fa1664cc00a
TakenouchiTR <draconianlds@gmail.com>
2021-10-26 12:16:21 -0400

    Add LabelSprite to project

---
+++ SpaceInvaders/View/UI/LabelSprite.xaml.cs
@@ -0,0 +1,26 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+using SpaceInvaders.Model;
+
+namespace SpaceInvaders.View.UI
+{
+    public sealed partial class LabelSprite
+    {
+        public string Text
+        {
+            get => this.textBlock.Text;
+            set => this.textBlock.Text = value;
+        }
+
+        public double TextWidth => this.textBlock.Width;
+
+        public double TextHeight => this.textBlock.Height;
+
+        public Vector2 TextSize => new Vector2(this.TextWidth, this.TextHeight);
+
+        public LabelSprite()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
