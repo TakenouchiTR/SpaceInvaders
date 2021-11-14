547131b9085bafd94aa6df498627f98873a8f1e3
TakenouchiTR <draconianlds@gmail.com>
2021-11-14 15:33:06 -0500

    Add ShieldSegment to prroject

---
+++ SpaceInvaders/Model/Nodes/Entities/ShieldSegment.cs
@@ -0,0 +1,29 @@
+﻿using SpaceInvaders.View.Sprites.Entities;
+
+namespace SpaceInvaders.Model.Nodes.Entities
+{
+    /// <summary>
+    ///     Represents one segment of the shield. Blocks bullets from both the player and the enemies.
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.Model.Nodes.Entities.Entity" />
+    public class ShieldSegment : Entity
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShieldSegment"/> class.
+        /// </summary>
+        public ShieldSegment() : base(new ShieldSegmentSprite())
+        {
+            Collision.CollisionLayers = PhysicsLayer.World;
+            Collision.CollisionMasks = PhysicsLayer.PlayerHitbox | PhysicsLayer.EnemyHitbox;
+            Collision.Monitoring = true;
+            Collision.Monitorable = true;
+
+            Collision.Collided += this.onCollision;
+        }
+
+        private void onCollision(object sender, CollisionArea e)
+        {
+            QueueForRemoval();
+        }
+    }
+}
