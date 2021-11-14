56b96d03ee3bc4359ecb6cef7fd700516e9e436c
TakenouchiTR <draconianlds@gmail.com>
2021-11-14 15:32:07 -0500

    Add ShieldSegmentSprite to project

---
+++ SpaceInvaders/View/Sprites/Entities/ShieldSegmentSprite.xaml.cs
@@ -0,0 +1,19 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.Entities
+{
+    /// <summary>
+    ///     Displays a segment of the shield
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.View.Sprites.BaseSprite" />
+    public sealed partial class ShieldSegmentSprite
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShieldSegmentSprite"/> class.
+        /// </summary>
+        public ShieldSegmentSprite()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
