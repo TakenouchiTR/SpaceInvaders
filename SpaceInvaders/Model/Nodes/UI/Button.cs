a849b703162443666051dea87e7e4b215db7c7b7
TakenouchiTR <draconianlds@gmail.com>
2021-11-23 16:20:28 -0500

    Add Button to the project

---
+++ SpaceInvaders/Model/Nodes/UI/Button.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using SpaceInvaders.View.UI;
+
+namespace SpaceInvaders.Model.Nodes.UI
+{
+    /// <summary>
+    ///     Draws a button
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.Model.Nodes.RenderableNode" />
+    public class Button : RenderableNode
+    {
+        #region Data members
+
+        private readonly ButtonSprite buttonSprite;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Button" /> class.<br />
+        ///     Precondition: None<br />
+        ///     Postcondition: this.Layer == RenderableNode.DefaultRenderLayer
+        /// </summary>
+        public Button() : this("")
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Button" /> class.<br />
+        ///     Precondition: None<br />
+        ///     Postcondition: this.Layer == SpriteNode.DefaultRenderLayer &amp;&amp;<br />
+        ///     this.Text == text &amp;&amp;<br />
+        ///     this.Visible == true
+        /// </summary>
+        /// <param name="text">The text.</param>
+        public Button(string text) : this(text, DefaultRenderLayer)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Button" /> class.<br />
+        ///     Precondition: None<br />
+        ///     Postcondition: this.Layer == SpriteNode.DefaultRenderLayer &amp;&amp;<br />
+        ///     this.Text == text &amp;&amp;<br />
+        ///     this.Visible == true
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="layer">The layer.</param>
+        public Button(string text, RenderLayer layer) : base(new ButtonSprite(), layer)
+        {
+            this.buttonSprite = (ButtonSprite) Sprite;
+            this.buttonSprite.Text = text;
+
+            this.buttonSprite.Button.Click += this.onButtonClick;
+            this.buttonSprite.Button.PointerEntered += this.onButtonPointerEntered;
+            this.buttonSprite.Button.PointerExited += this.onButtonPointerExited;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     Occurs when [click].
+        /// </summary>
+        public event EventHandler Click;
+
+        /// <summary>
+        ///     Occurs when [mouse enters the button area].
+        /// </summary>
+        public event EventHandler<Vector2> MouseEntered;
+
+        /// <summary>
+        ///     Occurs when [mouse exits the button area].
+        /// </summary>
+        public event EventHandler<Vector2> MouseExited;
+
+        /// <summary>
+        ///     Runs cleanup and invokes the Removed event when removed from the game.<br />
+        ///     Precondition: None<br />
+        ///     Postcondition: Removed event is invoked if emitRemovedEvent == true &amp;&amp;<br />
+        ///     All event subscribers are removed
+        /// </summary>
+        /// <param name="emitRemovedEvent">Whether to emit the Removed event</param>
+        public override void CompleteRemoval(bool emitRemovedEvent = true)
+        {
+            base.CompleteRemoval(emitRemovedEvent);
+
+            if (this.Click != null)
+            {
+                foreach (var subscriber in this.Click.GetInvocationList())
+                {
+                    this.Click -= subscriber as EventHandler;
+                }
+            }
+
+            if (this.MouseEntered != null)
+            {
+                foreach (var subscriber in this.MouseEntered.GetInvocationList())
+                {
+                    this.MouseEntered -= subscriber as EventHandler<Vector2>;
+                }
+            }
+
+            if (this.MouseExited != null)
+            {
+                foreach (var subscriber in this.MouseExited.GetInvocationList())
+                {
+                    this.MouseExited -= subscriber as EventHandler<Vector2>;
+                }
+            }
+
+            this.buttonSprite.Button.Click -= this.onButtonClick;
+            this.buttonSprite.Button.PointerEntered -= this.onButtonPointerEntered;
+            this.buttonSprite.Button.PointerExited -= this.onButtonPointerExited;
+        }
+
+        private void onButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Click?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void onButtonPointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            this.MouseEntered?.Invoke(this, Input.MousePosition);
+        }
+
+        private void onButtonPointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            this.MouseExited?.Invoke(this, Input.MousePosition);
+        }
+
+        #endregion
+    }
+}
---
+++ SpaceInvaders/View/UI/ButtonSprite.xaml.cs
@@ -0,0 +1,67 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+using Windows.UI.Xaml.Controls;
+
+namespace SpaceInvaders.View.UI
+{
+    /// <summary>
+    ///     Displays a button
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.View.Sprites.BaseSprite" />
+    public sealed partial class ButtonSprite
+    {
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets the text displayed in the button.
+        /// </summary>
+        /// <value>
+        ///     The text.
+        /// </value>
+        public string Text
+        {
+            get => this.button.Content?.ToString();
+            set => this.button.Content = value;
+        }
+
+        /// <summary>
+        ///     Gets or sets the button width.
+        /// </summary>
+        public double ButtonWidth
+        {
+            get => this.button.Width;
+            set => this.button.Width = value;
+        }
+
+        /// <summary>
+        ///     Gets or sets the button height.
+        /// </summary>
+        public double ButtonHeight
+        {
+            get => this.button.Height;
+            set => this.button.Height = value;
+        }
+
+        /// <summary>
+        ///     Gets the button.
+        /// </summary>
+        /// <value>
+        ///     The button.
+        /// </value>
+        public Button Button => this.button;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ButtonSprite" /> class.
+        /// </summary>
+        public ButtonSprite()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
