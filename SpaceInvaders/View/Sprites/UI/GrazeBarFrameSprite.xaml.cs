b3afe0398dd3fb5a63996ceef07f896eee7d08ed
TakenouchiTR <draconianlds@gmail.com>
2021-11-29 23:56:01 -0500

    Add GrazeBar and GrazeBar sprites to project

---
+++ SpaceInvaders/View/Sprites/UI/GrazeBarFrameSprite.xaml.cs
@@ -0,0 +1,31 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.UI
+{
+    /// <summary>
+    ///     Draws a frame for the Graze Bar
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.View.Sprites.BaseSprite" />
+    public sealed partial class GrazeBarFrameSprite
+    {
+        /// <summary>
+        ///     Sets the height of the bar as a percent of the max height.
+        /// </summary>
+        /// <value>
+        ///     The height of the bar.
+        /// </value>
+        public double BarHeight
+        {
+            set => this.bar.BarHeight = value;
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GrazeBarFrameSprite"/> class.
+        /// </summary>
+        public GrazeBarFrameSprite()
+        {
+            this.InitializeComponent();
+            this.BarHeight = 0;
+        }
+    }
+}
---
+++ SpaceInvaders/View/Sprites/UI/GrazeBarSprite.xaml.cs
@@ -0,0 +1,30 @@
+﻿// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace SpaceInvaders.View.Sprites.UI
+{
+    /// <summary>
+    ///     Draws a green bar to the screen
+    /// </summary>
+    /// <seealso cref="SpaceInvaders.View.Sprites.BaseSprite" />
+    public sealed partial class GrazeBarSprite
+    {
+        /// <summary>
+        ///     Sets the height of the bar as a percent of the max height.
+        /// </summary>
+        /// <value>
+        ///     The height of the bar.
+        /// </value>
+        public double BarHeight
+        {
+            set => this.bar.Height = this.Height * value;
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GrazeBarSprite"/> class.
+        /// </summary>
+        public GrazeBarSprite()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
